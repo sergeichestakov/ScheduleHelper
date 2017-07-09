f23231db403a9d8ab43cf96023c6814266052d0d
Sergei Chestakov <sergei332@gmail.com>
2017-07-08 17:53:27 -0700

    Moved files around

---
+++ src/inject.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	injectScript("src/conflict.js");
+	injectScript("src/inlineSearch.js");
+	injectScript("src/textSearch.js");
+	injectCSS("libs/balloon.min.css");
+});
+
+function injectCSS(fileName){
+	var body = document.getElementsByTagName("head")[0];
+	var sheet = document.createElement("link");
+	sheet.setAttribute("rel", "stylesheet");
+	sheet.setAttribute("type", "text/css");
+	sheet.setAttribute("href", chrome.extension.getURL(fileName));
+	body.appendChild(sheet);
+}
+
+function injectScript(fileName){
+	var body = document.getElementsByTagName("body")[0];
+	var script = document.createElement("script");
+	script.setAttribute("type", "text/javascript");
+	script.setAttribute("src", chrome.extension.getURL(fileName));
+	body.appendChild(script);
+}
