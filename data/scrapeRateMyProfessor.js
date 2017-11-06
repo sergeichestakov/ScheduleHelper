efed38b15b93ea4e01a204fd94100a30524ad42a
Sergei Chestakov <sergei332@gmail.com>
2017-11-06 02:39:40 -0800

    new scraper

--- data/scrapeRateMyProfessor.js
+++ data/scrapeRateMyProfessor.js
@@ -5,10 +5,14 @@ $.ajaxPrefilter( function (options) { // Injects a proxy into the browser so tha
     //options.url = "http://cors.corsproxy.io/url=" + options.url;
   }
 });
-// 4307
-var link, name, quality, i, setoff = 4080, count = 1, difficult, first, last, pageCount = 217;
+var link, name, quality, i, setoff = 0, count = 1, difficult, first, last, pageCount = 219;
 // http://www.ratemyprofessors.com/find/professor/?institution=University+of+California+Davis&page=1&query=Jelmer%20Eerkens&queryoption=TEACHER
 for (i=0; i < pageCount; i++) {
+  setTimeout(getPage(), 5000); //5 second delay
+}
+console.log("running");
+function getPage(){
+  console.log(i);
   $.get({
      async: false,
      url: 'http://www.ratemyprofessors.com/search.jsp?query=&queryoption=HEADER&stateselect=&country=&dept=&queryBy=teacherName&facetSearch=&schoolName=University+of+California+Davis&offset='+ setoff +'&max=50',
@@ -16,35 +20,26 @@ for (i=0; i < pageCount; i++) {
           data = $(response).find('.listings').children('li').each(function () {
              link = jQuery(this).children("a").attr('href');
              name = jQuery(this).find("span .main").text();
-
-
-                        $.ajax({
-                           async: false,
-                           url: 'http://www.ratemyprofessors.com' + link,
-                           success: function(data){
-                             quality = $(data).find(".grade")[0].innerText;
-                             difficult = $(data).find(".grade")[2].innerText;
-                             first = $(data).find(".pfname")[0].innerText;
-                             last = $(data).find(".plname")[0].innerText;
-                             console.log(count + "   " + name + " - Qual: " + quality + " - Diff: " + difficult + " - Link: " + link + " " + first + " " + last);
-
-                             var xmlhttp = new XMLHttpRequest();
-                             xmlhttp.onreadystatechange = function() {
-                                 if (this.readyState == 4 && this.status == 200) {}
-                             };
-                             xmlhttp.open("GET", "http://104.236.158.81/api/rateprofessorAPI.php?url=" + link + "&first=" + first.trim() + "&last=" + last.trim() + "&quality=" + quality + "&diff=" + difficult, true);
-                             xmlhttp.send();
-
-                           }
-                         });
-
-
-
+                $.ajax({
+                   async: false,
+                   url: 'http://www.ratemyprofessors.com' + link,
+                   success: function(data){
+                     quality = $(data).find(".grade")[0].innerText;
+                     difficult = $(data).find(".grade")[2].innerText;
+                     first = $(data).find(".pfname")[0].innerText;
+                     last = $(data).find(".plname")[0].innerText;
+                     console.log(count + "   " + name + " - Qual: " + quality + " - Diff: " + difficult + " - Link: " + link + " " + first + " " + last);
+                     var xmlhttp = new XMLHttpRequest();
+                     xmlhttp.onreadystatechange = function() {
+                         if (this.readyState == 4 && this.status == 200) {}
+                     };
+                     xmlhttp.open("GET", "http://104.236.158.81/api/rateprofessorAPI.php?url=" + link + "&first=" + first.trim() + "&last=" + last.trim() + "&quality=" + quality + "&diff=" + difficult, true);
+                     xmlhttp.send();
+                   }
+                 });
             count++;
           });
-          //console.log(data);
-          //$("#viewer").html(response);
-  }
+      setoff += 20;
+    }
   });
-  setoff += 20;
 }
---
+++ data/scrapeRatings.py
@@ -0,0 +1,51 @@
+import os
+import lxml.html
+import requests
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+
+search = 'http://www.ratemyprofessors.com/search.jsp?query=&queryoption=HEADER&stateselect=&country=&dept=&queryBy=teacherName&facetSearch=&schoolName=University+of+California+Davis&offset='
+pageCount = 219
+offset = 0
+count = 0
+
+def scrapeProf(professor):
+    #get their link and name
+    link = professor.find("a", href=True)['href']
+    name = professor.find("span", {"class": "main"}).text.split(',')
+    last = name[0].strip().encode('utf-8')
+    first = name[1].strip().encode('utf-8')
+
+    #open up each professors page to scrape their ratings
+    profLink = 'http://www.ratemyprofessors.com' + link
+    profPage = urlopen(profLink)
+    professorSoup = BeautifulSoup(profPage, "lxml")
+
+    ratings = professorSoup.find_all("div", {"class": "grade"})
+    overall = ratings[0].text.strip().encode('utf-8')
+    difficulty = ratings[2].text.strip().encode('utf-8')
+
+    #output
+    global count
+    count += 1
+    print(count, first, last, overall, difficulty)
+
+    #send over the data to populate db via get request
+    data = {'url':link, 'first':first,'last':last, 'quality':overall, 'diff':difficulty}
+    request = requests.get('https://getschedulehelper.com/api/rateprofessorAPI.php', params=data)
+
+def scrapePage(offset):
+    #open up the page
+    results = search + str(offset)
+    page = urlopen(results)
+    soup = BeautifulSoup(page, "html.parser")
+    listings = soup.find_all("li", {"class": "listing PROFESSOR"})
+
+    #loop through each professor in results
+    for professor in listings:
+        scrapeProf(professor)
+
+
+for index in range(pageCount):
+    scrapePage(offset)
+    offset += 20
