ecd0969763a66746dc6c6c23b71b5e1b220168bc
Sergei Chestakov <sergei332@gmail.com>
2018-01-23 08:55:40 -0800

    add checkfor results in scraper

--- data/scrapeRatings.py
+++ data/scrapeRatings.py
@@ -13,8 +13,9 @@ def scrapeProf(professor):
     #get their link and name
     link = professor.find("a", href=True)['href']
     name = professor.find("span", {"class": "main"}).text.split(',')
-    last = name[0].strip().encode('utf-8')
-    first = name[1].strip().encode('utf-8')
+    if len(name):
+        last = name[0].strip().encode('utf-8')
+        first = name[1].strip().encode('utf-8')
 
     #open up each professors page to scrape their ratings
     profLink = 'http://www.ratemyprofessors.com' + link
@@ -22,17 +23,19 @@ def scrapeProf(professor):
     professorSoup = BeautifulSoup(profPage, "lxml")
 
     ratings = professorSoup.find_all("div", {"class": "grade"})
-    overall = ratings[0].text.strip().encode('utf-8')
-    difficulty = ratings[2].text.strip().encode('utf-8')
+    if len(ratings) > 2:
+        overall = ratings[0].text.strip().encode('utf-8')
+        difficulty = ratings[2].text.strip().encode('utf-8')
 
     #output
     global count
     count += 1
-    print(count, first, last, overall, difficulty)
+    if overall:
+        print(count, first, last, overall, difficulty)
+        #send over the data to populate db via get request
+        data = {'url':link, 'first':first,'last':last, 'quality':overall, 'diff':difficulty}
+        request = requests.get('https://getschedulehelper.com/api/rateprofessorAPI.php', params=data)
 
-    #send over the data to populate db via get request
-    data = {'url':link, 'first':first,'last':last, 'quality':overall, 'diff':difficulty}
-    request = requests.get('https://getschedulehelper.com/api/rateprofessorAPI.php', params=data)
 
 def scrapePage(offset):
     #open up the page
